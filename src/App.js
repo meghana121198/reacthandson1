4beae096812e5edb822c8d1b5cd1b64845ff4352
meghana121198 <109966559+meghana121198@users.noreply.github.com>
2022-11-10 12:09:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Btnclk from './btnclk.jsx';
+function App() {
+  return (
+    <>
+    <Btnclk />
+    </>
+  );
+}
+
+export default App;
